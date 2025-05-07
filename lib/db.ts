d91139c12453ac46b65b3f492f70aafea67f8158
shinobialice <alice.but@heathmont.net>
2025-05-07 19:14:22 +0300

    fix

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,15 @@
+// lib/db.ts
 import { PrismaClient } from "@prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
+  prisma: ReturnType<PrismaClient["$extends"]> | undefined;
 };
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+const prismaClient = new PrismaClient().$extends(withAccelerate());
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+export const prisma = globalForPrisma.prisma ?? prismaClient;
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
