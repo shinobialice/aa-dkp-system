409508ec8da15b00b4721d79eb5f5738b8beaf51
shinobialice <alice.but@heathmont.net>
2025-05-11 23:00:24 +0300

    test

--- src/components/profile/notes/UserTagsSection.tsx
+++ src/components/profile/notes/UserTagsSection.tsx
@@ -8,7 +8,7 @@ import { Button } from "@/components/ui/button";
 import { Switch } from "@/components/ui/switch";
 import { updateUser } from "@/src/actions/updateUser";
 import { deleteUserTag, addUserTag } from "@/src/actions/userTagsActions";
-import useUserTag from "@/src/hooks/useUserTag";
+// import useUserTag from "@/src/hooks/useUserTag";
 import getSalaryEligibilityErrors from "@/src/utils/getSalaryEligibilityErrors";
 
 type UserType = {
@@ -58,7 +58,7 @@ export function UserTagsSection({
   averageGuildGS: number;
 }) {
   const [updating, setUpdating] = useState(false);
-  const isAdmin = useUserTag("Администратор");
+  // const isAdmin = useUserTag("Администратор");
 
   async function toggleActive(newValue: boolean) {
     setUpdating(true);
@@ -119,14 +119,14 @@ export function UserTagsSection({
         >
           Активен
         </Badge>
-        {isAdmin && (
-          <Switch
-            className="cursor-pointer"
-            checked={user.active}
-            onCheckedChange={toggleActive}
-            disabled={updating}
-          />
-        )}
+        {/* {isAdmin && ( */}
+        <Switch
+          className="cursor-pointer"
+          checked={user.active}
+          onCheckedChange={toggleActive}
+          disabled={updating}
+        />
+        {/* )} */}
       </div>
       <div className="flex justify-between items-center py-4">
         <Badge
@@ -135,14 +135,14 @@ export function UserTagsSection({
         >
           Получает зарплату
         </Badge>
-        {isAdmin && (
-          <Switch
-            checked={user.is_eligible_for_salary}
-            onCheckedChange={toggleSalary}
-            disabled={updating}
-            className="cursor-pointer"
-          />
-        )}
+        {/* {isAdmin && ( */}
+        <Switch
+          checked={user.is_eligible_for_salary}
+          onCheckedChange={toggleSalary}
+          disabled={updating}
+          className="cursor-pointer"
+        />
+        {/* )} */}
       </div>
 
       {tags.map((tag) => (
@@ -155,37 +155,37 @@ export function UserTagsSection({
           >
             {tag.tag}
           </Badge>
-          {isAdmin && (
-            <Button
-              variant="ghost"
-              size="icon"
-              onClick={() => handleDeleteTag(tag.id)}
-              className="text-muted-foreground cursor-pointer"
-            >
-              <Trash2 />
-            </Button>
-          )}
+          {/* {isAdmin && ( */}
+          <Button
+            variant="ghost"
+            size="icon"
+            onClick={() => handleDeleteTag(tag.id)}
+            className="text-muted-foreground cursor-pointer"
+          >
+            <Trash2 />
+          </Button>
+          {/* )} */}
         </div>
       ))}
-      {isAdmin && (
-        <div>
-          {availableTags.length > 0 && (
-            <div className="flex flex-wrap gap-2 pt-4">
-              {availableTags.map((tag) => (
-                <Button
-                  key={tag}
-                  variant="outline"
-                  onClick={() => handleAddTag(tag)}
-                  className="text-sm cursor-pointer"
-                >
-                  <CirclePlus className="w-4 h-4 mr-1" />
-                  {tag}
-                </Button>
-              ))}
-            </div>
-          )}
-        </div>
-      )}
+      {/* {isAdmin && ( */}
+      <div>
+        {availableTags.length > 0 && (
+          <div className="flex flex-wrap gap-2 pt-4">
+            {availableTags.map((tag) => (
+              <Button
+                key={tag}
+                variant="outline"
+                onClick={() => handleAddTag(tag)}
+                className="text-sm cursor-pointer"
+              >
+                <CirclePlus className="w-4 h-4 mr-1" />
+                {tag}
+              </Button>
+            ))}
+          </div>
+        )}
+      </div>
+      {/* )} */}
     </div>
   );
 }
